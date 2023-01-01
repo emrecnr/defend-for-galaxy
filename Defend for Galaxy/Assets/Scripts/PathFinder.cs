67b6527095c218cd86c11e54ba1ef2b760dec836
platanusdev <platanusdev@gmail.com>
2023-01-01 17:41:19 +0300

    Pathfinder uygulandı

---
+++ Defend for Galaxy/Assets/Scripts/PathFinder.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinder : MonoBehaviour
+{
+    [SerializeField] WaveConfig waveConfig;
+    List<Transform> waypoints;
+    int waypointIndex = 0;
+
+
+    void Start()
+    {
+        waypoints = waveConfig.GetWayPoints();
+        transform.position = waypoints[waypointIndex].position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowPath();
+    }
+
+    void FollowPath()
+    {
+        if (waypointIndex<waypoints.Count)
+        {
+            Vector3 targetPosition = waypoints[waypointIndex].position;
+            float delta = waveConfig.GetMoveSpeed()*Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, delta);
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+            else
+            {
+               // Destroy(gameObject);
+            }
+        }
+    }
+
+
+
+}
